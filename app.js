09d9b32b4a0e43a841559b7d415768ec1d6cf2d7
seniya <seniyathri@gmail.com>
2024-03-14 10:05:52 +0100

    feat: if statment has been updated with the vairaiable total

--- app.js
+++ app.js
@@ -7,13 +7,14 @@ for (let i = 0; i < 100; i++) {
   const resto2 = num % 2;
   const resto3 = num % 3;
   const resto5 = num % 5;
+  const total = num % 3 && num % 5;
   // console.log(num, resto);
 
   if (resto3 === 0) {
     console.log("Fizz");
   } else if (resto5) {
     console.log("buz");
-  } else if (resto3 === 0 && resto5 === 0) {
+  } else if (total === 0) {
     console.log("FizzBuzz");
   } else {
     console.log(num);
